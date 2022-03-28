3679cb65ff91edcff2611ff79b2c8d42e70ad250
SSuSG <psg980331@naver.com>
2022-03-28 18:32:11 +0900

    dd

---
+++ front/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    
+    <header-bar></header-bar>
+    
+  
+    <v-content>
+      <travel-map></travel-map>
+    </v-content>
+    
+  </v-app>
+</template>
+
+<script>
+import HeaderBar from './components/HeaderBar.vue'
+import TravelMap from './components/TravelMap.vue'
+
+export default {
+  name: 'app',
+  components:{
+    HeaderBar,
+    TravelMap
+  }
+
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ front/src/components/MenuBar.vue
@@ -0,0 +1,63 @@
+<template>
+   <v-menu offset-y  :close-on-content-click="false">
+      <template v-slot:activator="{ on: menu, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="menu"
+        >
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </template>
+      
+        <v-list >
+        
+          <v-list-item-content>
+              <v-list-item-title @click="screenChange()">Travel작성</v-list-item-title>
+          </v-list-item-content>
+
+          <rank-comp></rank-comp>
+          
+          <v-list-item-content>
+              <v-list-item-title @click="screenChange()">MyPage</v-list-item-title>
+          </v-list-item-content>
+        
+        </v-list>
+
+      
+
+      
+    </v-menu>
+</template>
+
+<script>
+import RankComp from './RankComp.vue'
+
+
+export default {
+  components: { RankComp },
+    name : 'MenuBar',
+    data () {
+        return {
+            items: [
+                { title: 'Travel작성' },
+                { title: '랭킹' },
+                { title: 'MyPage' },
+                
+            ]
+        }
+    } ,
+    component :{
+      RankComp
+    },
+    methods: {
+        screenChange(){
+            console.log("click")
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>
