8b9b5caccc5a5c6740d5659fa0944a09d8476976
SSuSG <psg980331@naver.com>
2022-03-28 18:32:11 +0900

    dd

---
+++ front/src/components/HeaderBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-div>
+    
+    <v-app-bar dense>
+        <menu-bar ></menu-bar>
+        <v-toolbar-title>GaBom</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <join-comp></join-comp>
+        <login-comp/>
+    </v-app-bar>
+</v-div>
+</template>
+
+<script>
+import LoginComp from './LoginComp.vue'
+import JoinComp from './JoinComp.vue'
+import MenuBar from './MenuBar.vue'
+
+export default {
+    name: 'HeaderBar',
+    components:{
+        LoginComp,JoinComp,MenuBar
+    }
+}
+</script>
