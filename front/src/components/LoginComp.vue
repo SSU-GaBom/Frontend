e4d54388789cacafbb1ae6795c8326061d9e5aaf
SSuSG <psg980331@naver.com>
2022-04-01 13:09:48 +0900

    join

--- front/src/components/LoginComp.vue
+++ front/src/components/LoginComp.vue
@@ -15,28 +15,30 @@
 
         
       <v-sheet min-height="500">
-      <div class="text-center"><img src="../assets/svcLogoWithoutFrames.svg" class="img-fluid pa-3 my-5" alt="Logo" width="200" height="50"/></div>
+        <div class="text-center"><img src="../assets/svcLogoWithoutFrames.svg" class="img-fluid pa-3 my-5" alt="Logo" width="200" height="50"/></div>
       <v-sheet class="headline mb-2 text-center"> 로그인 </v-sheet>
+
       <v-card
-      class="mx-auto px-5 ma-5"
-      max-width="500"
-      outlined>
-    <v-form
-    ref="form"
-    v-model="hasError"
-    lazy-validation
-    class="pa-5 ma-5"
-  >
-    <v-text-field
-      v-model="form.username"
-      :counter="20"
-      :rules="valid.username"
-      label="아이디"
-      placeholder="6글자 이상, 영문 대/소문자 및 숫자"
-      required
-      maxlength="20"
-    ></v-text-field>
-    <v-text-field
+        class="mx-auto px-5 ma-5"
+        max-width="500"
+        outlined>
+      <v-form
+        ref="form"
+        v-model="hasError"
+        lazy-validation
+        class="pa-5 ma-5"
+      >
+      <v-text-field
+        v-model="form.username"
+        :counter="20"
+        :rules="valid.username"
+        label="아이디"
+        placeholder="6글자 이상, 영문 대/소문자 및 숫자"
+        required
+        maxlength="20"
+      ></v-text-field>
+
+      <v-text-field
             v-model="form.password"
             :append-icon="options.passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
             :rules="valid.password"
@@ -47,14 +49,14 @@
             counter
             required
             class="mb-5"
-        
           />
-          
-  </v-form>
+        
+        </v-form>
       </v-card>
+
       <v-sheet class="headline mb-2 text-center">
-      <v-btn color="primary" outlined :disabled="!form1OK" class="mr-2" >로그인</v-btn>
-      <v-btn color="grey darken-2" @click="dialog=false" outlined>창닫기</v-btn>
+        <v-btn color="primary" outlined :disabled="!form1OK" class="mr-2" >로그인</v-btn>
+        <v-btn color="grey darken-2" @click="dialog=false" outlined>창닫기</v-btn>
       </v-sheet>
     </v-sheet>
       
--- src/main/java/GaBom/Bom/TestController.java
+++ src/main/java/GaBom/Bom/TestController.java
@@ -1,11 +1,13 @@
 package GaBom.Bom;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
@@ -21,5 +23,13 @@ public class TestController {
     public String test(@RequestBody Test test){
         testService.save(test);
         return "success";
+    }
+
+    @PostMapping("/api/join")
+    public String joinUserTest(@RequestBody TestDto testDto){
+        log.info("joinUserTest");
+        Test test = testDto.toEntity();
+        testService.save(test);
+        return "JoinUserTest Success";
     }
 }
