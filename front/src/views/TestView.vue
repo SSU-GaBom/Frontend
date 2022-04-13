984df919d7925b932f19847a2a773bf5eb322a7d
SSuSG <psg980331@naver.com>
2022-04-13 15:06:38 +0900

    add testview

---
+++ front/src/views/TestView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <div>{{userName}}</div>
+      <div>{{userEmail}}</div>
+  </div>
+
+</template>
+
+<script>
+import {testUser} from '../api/index'
+
+export default {
+    name: 'TestView',
+    data () {
+        return {
+            userName : null,
+            userEmail : null,
+
+        }
+    },
+    methods: {
+        async testUserData() {
+            const response = await testUser();
+            console.log(response)
+
+        }
+    },
+    created() {
+
+    },
+    components: {
+    },
+  }
+</script>
