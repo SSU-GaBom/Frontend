c39cc1ed80ad0933348399aba536b4b6e8571c78
rlatldhs32 <rlatldhs32.naver.com>
2022-04-18 23:04:20 +0900

    220418 Update

--- src/main/java/GaBom/Bom/service/TravelService.java
+++ src/main/java/GaBom/Bom/service/TravelService.java
@@ -7,7 +7,10 @@ import GaBom.Bom.entity.User;
 import GaBom.Bom.repository.TravelRepository;
 import GaBom.Bom.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +27,7 @@ public class TravelService {
     @Transactional
     public boolean save(String username, Travel travel){
         User user = userRepository.findByUserName(username);
-          //만약 userName이 없으면? -> 에러처리. 근데 로그인세션으로 할거니까 그때 보고?하기.
+        //만약 userName이 없으면? -> 에러처리. 근데 로그인세션으로 할거니까 그때 보고?하기.
         user.add(travel);
         travelRepository.save(travel);
         return true;
@@ -45,15 +48,15 @@ public class TravelService {
         return true;
     }
 
-    public List<Travel> FindTravelsBypageRequest(Pageable pageable){
-        return travelRepository.findAll(pageable).getContent();
-    }
 
-    public List<Travel> FindTravels(){
-        return travelRepository.findAll();
+    public Page<Travel> TravelsByLikeCount(int Page,int Size){
+        PageRequest pageRequest = PageRequest.of(Page, Size, Sort.by(Sort.Direction.DESC, "likedCount"));
+        Page<Travel> memberPages = travelRepository.findAll(pageRequest);
+        return memberPages;
     }
 
 //    Pageable sortedByPriceDesc = PageRequest.of(0, 3, Sort.by("likeCount").descending());
+
 
 
     @Transactional
