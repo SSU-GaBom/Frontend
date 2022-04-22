8f7d18e7a8a7e3388445000c5e42b6f019f846d3
dogfooter <yoon971224@gmail.com>
2022-04-22 23:39:00 +0900

    some update for pull

---
+++ front/src/views/WriteTravel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+      <select-location/>
+  </div>
+</template>
+
+<script>
+import SelectLocation from '../components/SelectLocation.vue'
+
+export default {
+  name: 'WriteTravel',
+  components: {
+    SelectLocation
+  },
+}
+</script>
+
+<style scoped>
+#main {
+    position: relative;
+    width: 30%;
+    height: 580px;
+}
+#content {
+  margin: 10px auto;
+}
+</style>
