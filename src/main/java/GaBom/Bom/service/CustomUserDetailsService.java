6ec4ed27da4e57cc6ef9b7b739e625c4af6381d9
yongdori00 <yongdori98@gmail.com>
2022-04-27 11:11:44 +0900

    commit

--- src/main/java/GaBom/Bom/service/CustomUserDetailsService.java
+++ src/main/java/GaBom/Bom/service/CustomUserDetailsService.java
@@ -17,8 +17,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUserId(username).orElseThrow(CUserNotFoundException::new);
-
-        return user;
+        return userRepository.findByUserId(username).orElseThrow(CUserNotFoundException::new);
     }
 }
